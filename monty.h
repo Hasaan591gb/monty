9790b597e4fb114198a250de1eb6f8aee1c8c748
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-25 13:58:45 -0700

    added function protype for div

--- monty.h
+++ monty.h
@@ -47,5 +47,6 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
+void div(stack_t **stack, unsigned int line_number);
 
 #endif
