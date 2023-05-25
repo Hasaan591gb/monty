a989e517688417a0bf55b6ed3ad3cb6cb82f08d2
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-25 15:51:28 -0700

    included function prototype for rotr

--- monty.h
+++ monty.h
@@ -53,5 +53,6 @@ void modu(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
 
 #endif
