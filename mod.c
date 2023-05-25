ee2ead122dda8f3f1a57c7e479dafa43d0471533
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-25 14:33:15 -0700

    c function that computes the rest of the division of the second top element of the stack by the top element of the stack

---
+++ mod.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+ * modu - computes the remainder of the division of the second top element
+ * of the stack by the top element
+ * @stack: double pointer to the top of the stack
+ * @line_number: line number of the current operation
+ *
+ * Description: The result is stored in the second top element of the stack,
+ * and the top element is removed.
+ */
+void modu(stack_t **stack, unsigned int line_number)
+{
+	stack_t *local_stack;
+
+	if (*stack == NULL  || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n %= (*stack)->n;
+	local_stack = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(local_stack);
+}
